df89c0a616e5dac8de1d71844fd78d05999f5964
Namletapcode <namlecyb@gmail.com>
2025-03-17 17:05:50 +0700

    Add files via upload

---
+++ bullet.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Bullet:
+    def __init__(self, game, x, y):
+        self.screen = game.screen
+        self.settings = game.settings
+        self.x = x
+        self.y = y
+        self.rect = pygame.Rect(self.x, self.y, 3, 10)
+
+    def update(self):
+        self.y -= 10
+        self.rect.y = self.y
+
+    def draw(self):
+        pygame.draw.rect(self.screen, "yellow", self.rect)
