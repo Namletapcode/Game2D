8fbcb34d70cefd9c5ea4c2ef0e7a941935f63fa9
Namletapcode <namlecyb@gmail.com>
2025-03-17 16:44:00 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__ (self):
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.fps = 60
+        self.dt_max = 3/self.fps
+        self.player_speed = 2
