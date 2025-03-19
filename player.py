da1104c4c0d59217568b4b02bbb5b3708f2d710d
henrivu1285 <minecraftteam125@gmail.com>
2025-03-19 13:54:41 +0700

    Update nhân vật + va chạm

--- player.py
+++ player.py
@@ -7,11 +7,14 @@ class Player(pygame.sprite.Sprite):
         self.settings=Settings()
         self.screen=game.screen
         self.screen_rect=game.screen_rect
-        self.rect=pygame.Rect(self.settings.screen_width//2,self.settings.screen_height-40,30,30)
+        #self.rect=pygame.Rect(self.settings.screen_width//2,self.settings.screen_height-40,30,30)
         self.direction=pygame.Vector2(0,0)
         self.bullets=[]
+        self.radius=5
+        self.x=self.settings.screen_width//2
+        self.y=self.settings.screen_height-40
     def draw_player(self):
-        pygame.draw.rect(self.screen,(255,0,0),self.rect)
+        pygame.draw.circle(self.screen,(255,0,0),(self.x,self.y),self.radius)
     def update_player(self):
         self.input()
         self.move()
@@ -27,22 +30,27 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = -1
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
-        else: self.direction.y=0 
+        else: self.direction.x=0 
         if keys[pygame.K_UP]:
             self.direction.y = -1
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
         else: self.direction.y=0
+      
     def move(self):
          if self.direction.x and self.direction.y:
              self.direction.normalize()
-         self.rect.x+=self.direction.x * self.settings.player_speed
-         self.rect.y+=self.direction.y* self.settings.player_speed
-    def shoot(self):
-        bullet1=Bullet(self,self.rect.x,self.rect.y)
-        bullet2=Bullet(self,self.rect.x+10,self.rect.y)
-        bullet3=Bullet(self,self.rect.x+20,self.rect.y)
-        self.bullets.append(bullet1)
-        self.bullets.append(bullet2)
-        self.bullets.append(bullet3)
+         self.x+=self.direction.x * self.settings.player_speed
+         self.y+=self.direction.y * self.settings.player_speed
+    def handle_screen_collision(self):
+        """Ngăn hình tròn đi ra ngoài màn hình"""
+        if self.x - self.radius < 0:
+            self.x = self.radius  # Giữ trong giới hạn trái
+        if self.x + self.radius > self.settings.screen_width:
+            self.x = self.settings.screen_width - self.radius  # Giữ trong giới hạn phải
+        if self.y - self.radius < 0:
+            self.y = self.radius  # Giữ trong giới hạn trên
+        if self.y + self.radius > self.settings.screen_height:
+            self.y = self.settings.screen_height - self.radius
+   
         
